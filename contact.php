cb9f2f1244e8a35d7d933ecc1092613ec33fcf8b
Riyad Preukschas <riyad@informatik.uni-bremen.de>
2010-10-27 17:09:29 +0200

    Redid contact.php using parts from contribute.php.

--- contact.php
+++ contact.php
@@ -6,18 +6,73 @@
 ?>
 <?php echoH2(); ?>
 
-    <dl>
-      <dt><? emailLink("Send an e-mail to the maintainer") ?></dt>
-        <dd>BasKet is currently maintained by <a href="mailto:kelvie@ieee.org">Kelvie Wong</a>.</dd>
-	<dd>BasKet Note Pads was originally authored and maintained by <a href="mailto:slaout@linux62.org">Sébastien Laoût</a> who has since stepped down.</dd>
-	<dd>To report a bug, a suggestion, a wish, propose a patch, a package, or a port, use the <a href="likeback.php">LikeBack</a> interface in BasKet, or post it to the <a href="bugs/">bug tracker</a>.</dd>
-      <dt><a href="mailinglist.php">Development Mailing List</a></dt>
-        <dd>Development decisions take place between developers by emails on this list; feel free to join in even if you aren't a developer!</dd>
-      <dt><a href="http://kde-apps.org/content/show.php?content=10020">Add a comment on BasKet Note Pads's KDE-Apps.org page</a></dt>
-        <dd>KDE-Apps.org site allows you to post comments. This allows other people interested in your comments to see and reply to them. The site also allows you to vote for applications. Vote for BasKet Note Pads if you like it;</dd>
-      <dt>Help with BasKet development</dt>
-        <dd>You don't need to be a programmer to help make BasKet Note Pads a great application.<br>
-          I'm searching for translators, packagers, artists, and bug reporters. Just contact me and I will be happy to include your contribution to BasKet Note Pads.</dd>
-    </dl>
+<?php /* from contribute.php */ ?>
+<h3>Questions, Praise, Comments</h3>
+  <h4>
+    <img  src="icons/mail-to-maintainer.png" alt="" width="32" height="32" />
+    Mail The Maintainer
+  </h4>
+  <p>
+    For quick help or immediate contanct you can write a mail to the maintainer.
+  </p>
+  <p>
+    BasKet is currently maintained by <a href="mailto:kelvie@ieee.org">Kelvie Wong</a>.
+  </p>
+
+  <h4>
+    <img  src="icons/irc.png" alt="" width="32" height="32" />
+    Join us in IRC
+  </h4>
+  <p>
+    For fast help or a quick chat you can meet the developers on the #basket-devel channel on the irc.freenode.net server.
+  </p>
+
+  <h4>
+    <img  src="icons/kde-apps.org.png" alt="" width="32" height="32" />
+    Visit us on <a href="http://kde-apps.org">KDE-Apps.org</a>
+  </h4>
+  <p>
+    KDE-Apps.org is a directory for KDE applications so naturally you can <a href="http://kde-apps.org/content/show.php?content=10020" title="BasKet Note Pads on KDE-Apps.org">find BasKet Note Pads there</a>.
+  </p>
+  <p>
+    There you can get in contanct with other BasKet users and comment on the application itself.
+    You can also vote BasKet (up or down), so please vote and let us and other users know how you feel.
+  </p>
+
+
+<h3>Discussions</h3>
+  <h4>
+    <img  src="icons/mailinglist.png" alt="" width="32" height="32" />
+    Development Mailing List
+  </h4>
+  <p>
+    Development discussions take place in public.
+    If you are interested in following or contributing to those discussions this is the place to be.
+    Discussions generally revolve around things like
+  </p>
+  <ul>
+    <li>User support</li>
+    <li>Possible new features</li>
+    <li>Finding and fixing bugs</li>
+    <li>Release planing</li>
+    <li>And general management</li>
+  </ul>
+  <p>
+    To get a look at past topics you can <a href="http://sourceforge.net/mailarchive/forum.php?forum_name=basket-devel">browse the archives</a>.
+  </p>
+  <p>
+    To join the discussions all you have to do is to <a href="https://lists.sourceforge.net/lists/listinfo/basket-devel">subscribe to the mailing list</a> to be able to <a href="mailto:basket-devel@lists.sourceforge.net">send emails</a> there.
+  </p>
+<?php /* end from contribute.php */ ?>
+
+<h3>Contribute</h3>
+  <p>
+    You don't need to be a programmer to help make BasKet Note Pads a great application.<br>
+    We are searching for translators, packagers, artists, and bug reporters.
+    Just contact us and we will be happy to include your contribution to BasKet Note Pads.
+  </p>
+  <p>
+    For more information how to participate and contribute please have a look at our <a href="contribute.php">contribution page</a>.
+  </p>
 
 <?php include('includes/footer.php'); ?>
